fb5d25f89bebfb6ff490fba3777fd3a1f1707f53
Marcos Gallardi <gallardimarcos98@gmail.com>
2023-07-11 09:01:46 -0300

    commit 1 de prueba

---
+++ index.js
@@ -0,0 +1,11 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+
+
+conn.sync({ alter: true }).then(() => {
+
+  server.listen(3001, () => {
+    console.log("%s listening at 3001"); 
+  });
+});
