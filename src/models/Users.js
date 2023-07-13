cd51e3dbc7e02c2a12b58a3297cfa77c8d89e0c8
Cesar Aguado <cesaraguado123@gmail.com>
2023-07-12 20:00:32 -0500

    ready model users

---
+++ src/models/Users.js
@@ -0,0 +1,51 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+
+module.exports = (sequelize) => {
+  
+  sequelize.define("Users", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+           
+    },
+    lastName:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+        isEmail: true,
+                  },
+        },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+          
+      },
+      birthDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+         is: /^\+?[0-9\-()\s]+$/, 
+            },
+      }
+  },
+  { 
+    timestamps: false
+  });
+};
