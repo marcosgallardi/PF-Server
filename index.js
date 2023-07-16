c78ec1c0e968a53e866f3262a117c878e7820da7
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-16 11:26:05 -0300

    preparando para deploy

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+const port = process.env.PORT || 3001;
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001");
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`);
   });
 });
