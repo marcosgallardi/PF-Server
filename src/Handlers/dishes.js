fd6c0824e6aa5d64f8a387484e444bc6c1d3f051
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-16 19:30:56 -0300

    prueba pruebas searchbyname

--- src/Handlers/dishes.js
+++ src/Handlers/dishes.js
@@ -1,22 +1,23 @@
 const getAllDishes = require("../Controllers/getAllDishes");
-const searchByName = require("../Controllers/searchByName");
+const getByName = require("../Controllers/getByName");
 
 const dishes = async (req, res) => {
   const name = req.query;
   if (name) {
     try {
-      const dishes = await searchByName(name);
+      const dishes = await getByName(name);
       res.status(200).json(dishes);
     } catch (error) {
       res.status(200).json({ error: error.message });
     }
-  } else
+  } else if (name === undefined) {
     try {
       const dishes = await getAllDishes();
       res.status(200).json(dishes);
     } catch (error) {
       res.status(200).json({ error: error.message });
     }
+  }
 };
 
 module.exports = dishes;
