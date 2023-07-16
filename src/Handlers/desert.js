3bd49ad9ed0a02c45385442ad3f34f8200ceb44c
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-16 19:11:44 -0300

    prueba rutas nuevas y query en cada getall

--- src/Handlers/desert.js
+++ src/Handlers/desert.js
@@ -1,11 +1,22 @@
 const getAllDeserts = require("../Controllers/getAllDeserts");
+const getByName = require("../Controllers/getByName");
 
 const desert = async (req, res) => {
-  try {
-    const deserts = await getAllDeserts();
-    res.status(200).json(deserts);
-  } catch (error) {
-    res.status(200).json({ error: error.message });
+  const name = req.query;
+  if (name) {
+    try {
+      const deserts = await getByName(name);
+      res.status(200).json(deserts);
+    } catch (error) {
+      res.status(200).json({ error: error.message });
+    }
+  } else {
+    try {
+      const deserts = await getAllDeserts();
+      res.status(200).json(deserts);
+    } catch (error) {
+      res.status(200).json({ error: error.message });
+    }
   }
 };
 
--- src/routes/gralRouter.js
+++ src/routes/gralRouter.js
@@ -2,10 +2,8 @@ const { Router } = require("express");
 
 const gralRouter = Router();
 const searchByName = require("../Handlers/searchByName");
-const searchById = require('../Handlers/searchById')
+const searchById = require("../Handlers/searchById");
 
-
-gralRouter.get("/", searchByName);
-gralRouter.get("/:id",searchById);
+gralRouter.get("/:id", searchById);
 
 module.exports = gralRouter;
