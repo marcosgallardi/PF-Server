e86734f8f31eaa61a2948752650b4403c23bbe45
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-22 11:11:38 -0300

    swagger off

--- src/routes/desertRouter.js
+++ src/routes/desertRouter.js
@@ -3,88 +3,78 @@ const desert = require("../Handlers/desert");
 const desertRouter = Router();
 const createDesert = require("../Handlers/createDesert");
 
+// /**
+//  * @swagger
+//  * components:
+//  *  schemas:
+//  *    Desert:
+//  *      type: object
+//  *      properties:
+//  *          id:
+//  *           type: string
+//  *           format: uuid
+//  *           description: desert id
+//  *          name:
+//  *           type: string
+//  *           description: desert name
+//  *          stock:
+//  *           type: integer
+//  *           description: desert stock
+//  *          price:
+//  *           type: integer
+//  *           description: desert price
+//  *          image:
+//  *           type: string
+//  *           description: desert image
+//  *       required:
+//  *        - name
+//  *        - stock
+//  *        - price
+//  *       example:
+//  *          name: helado
+//  *          stock: 20
+//  *          price: 1500
+//  */
 
-/**
- * @swagger
- * components:
- *  schemas:
- *    Desert:
- *      type: object
- *      properties:
- *          id:
- *           type: string
- *           format: uuid 
- *           description: desert id
- *          name:
- *           type: string
- *           description: desert name
- *          stock:
- *           type: integer
- *           description: desert stock
- *          price:
- *           type: integer
- *           description: desert price
- *          image: 
- *           type: string
- *           description: desert image
- *       required:
- *        - name
- *        - stock
- *        - price
- *       example:
- *          name: helado
- *          stock: 20
- *          price: 1500
- */
-
-/**
- * @swagger
- * /desert:
- *  get:
- *    summary: bring desserts by name or all
- *    tags: [Desert]
- *    responses: 
- *      200:
- *        description: returns dessert by name or all
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Desert'
- */
+// /**
+//  * @swagger
+//  * /desert:
+//  *  get:
+//  *    summary: bring desserts by name or all
+//  *    tags: [Desert]
+//  *    responses:
+//  *      200:
+//  *        description: returns dessert by name or all
+//  *        content:
+//  *          application/json:
+//  *            schema:
+//  *              type: array
+//  *              items:
+//  *                $ref: '#/components/schemas/Desert'
+//  */
 desertRouter.get("/", desert);
 
 desertRouter.get("/:id", (req, res) => {
   res.status(200).send("obtengo el desert de id ");
 });
 
-
-/**
- * @swagger
- * /desert:
- *  post:
- *    summary: create a new dessert
- *    tags: [Desert]
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Desert'
- *    responses: 
- *      200:
- *        description: successfully created dessert
- */
+// /**
+//  * @swagger
+//  * /desert:
+//  *  post:
+//  *    summary: create a new dessert
+//  *    tags: [Desert]
+//  *    requestBody:
+//  *      required: true
+//  *      content:
+//  *        application/json:
+//  *          schema:
+//  *            $ref: '#/components/schemas/Desert'
+//  *    responses:
+//  *      200:
+//  *        description: successfully created dessert
+//  */
 
 desertRouter.post("/", createDesert);
 
 module.exports = desertRouter;
-
-
-
-
-
-
-
-
--- src/routes/sideRouter.js
+++ src/routes/sideRouter.js
@@ -4,85 +4,85 @@ const sideRouter = Router();
 const sides = require("../Handlers/sides");
 const createSide = require("../Handlers/createSide");
 
-/**
- * @swagger
- * components:
- *  schemas:
- *    Side:
- *      type: object
- *      properties:
- *          id:
- *           type: string
- *           format: uuid 
- *           description: side id
- *          name:
- *           type: string
- *           description: side name
- *          type:
- *           type: string
- *           description: side type
- *          available:
- *           type: boolean
- *           description: side available
- *          price:
- *           type: integer
- *           description: side price
- *          image: 
- *           type: string
- *           description: side image
- *       required:
- *        - name
- *        - type
- *        - available
- *        - price
- *       example:
- *          name: napolitana
- *          type: salsa
- *          available: true
- *          price: 1500
- *            
- *           
- *            
- */
+// /**
+//  * @swagger
+//  * components:
+//  *  schemas:
+//  *    Side:
+//  *      type: object
+//  *      properties:
+//  *          id:
+//  *           type: string
+//  *           format: uuid
+//  *           description: side id
+//  *          name:
+//  *           type: string
+//  *           description: side name
+//  *          type:
+//  *           type: string
+//  *           description: side type
+//  *          available:
+//  *           type: boolean
+//  *           description: side available
+//  *          price:
+//  *           type: integer
+//  *           description: side price
+//  *          image:
+//  *           type: string
+//  *           description: side image
+//  *       required:
+//  *        - name
+//  *        - type
+//  *        - available
+//  *        - price
+//  *       example:
+//  *          name: napolitana
+//  *          type: salsa
+//  *          available: true
+//  *          price: 1500
+//  *
+//  *
+//  *
+//  */
 
-/**
- * @swagger
- * /side:
- *  get:
- *    summary: bring all side
- *    tags: [Side]
- *    responses: 
- *      200:
- *        description: returns all side
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Side'
- */
+// /**
+//  * @swagger
+//  * /side:
+//  *  get:
+//  *    summary: bring all side
+//  *    tags: [Side]
+//  *    responses:
+//  *      200:
+//  *        description: returns all side
+//  *        content:
+//  *          application/json:
+//  *            schema:
+//  *              type: array
+//  *              items:
+//  *                $ref: '#/components/schemas/Side'
+//  */
 sideRouter.get("/", sides);
 
 sideRouter.get("/:id", (req, res) => {
   res.status(200).send("obtengo el side de id ");
 });
 
-/**
- * @swagger
- * /side:
- *  post:
- *    summary: create a new side
- *    tags: [Side]
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Side'
- *    responses: 
- *      200:
- *        description: successfully created Side
- */
+// /**
+//  * @swagger
+//  * /side:
+//  *  post:
+//  *    summary: create a new side
+//  *    tags: [Side]
+//  *    requestBody:
+//  *      required: true
+//  *      content:
+//  *        application/json:
+//  *          schema:
+//  *            $ref: '#/components/schemas/Side'
+//  *    responses:
+//  *      200:
+//  *        description: successfully created Side
+//  */
 
 sideRouter.post("/", createSide);
 
