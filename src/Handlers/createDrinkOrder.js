645caad7b9c3002aa9d9223660e88640d40b58ec
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-25 20:52:42 -0300

    mail funcionando, pero dentro de postdrinkorder, provisorio para demon

--- src/Handlers/createDrinkOrder.js
+++ src/Handlers/createDrinkOrder.js
@@ -1,4 +1,5 @@
 const postDrinkOrder = require("../Controllers/postDrinkOrder");
+const mailCreate = require("../Controllers/mailCreate");
 
 const createDrinkOrder = async (req, res) => {
   const { userId, drinkId, quantity, unitaryPrice, totalPrice } = req.body;
@@ -6,7 +7,7 @@ const createDrinkOrder = async (req, res) => {
   try {
     const drinkOrder = { userId, drinkId, quantity, unitaryPrice, totalPrice };
     const newDrinkOrder = await postDrinkOrder(drinkOrder);
-
+    mailCreate(drinkId, userId);
     return res.status(201).json(newDrinkOrder);
   } catch (error) {
     return res.status(200).json({ error: error.message });
