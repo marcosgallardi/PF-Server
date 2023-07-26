c7daff7dab3e18d858688db7f72068aa16ee640c
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-25 23:54:07 -0300

    borre todo lo del mail

--- src/Handlers/createDrinkOrder.js
+++ src/Handlers/createDrinkOrder.js
@@ -1,5 +1,4 @@
 const postDrinkOrder = require("../Controllers/postDrinkOrder");
-const mailCreate = require("../Controllers/mailCreate");
 
 const createDrinkOrder = async (req, res) => {
   const { userId, drinkId, quantity, unitaryPrice, totalPrice } = req.body;
@@ -7,7 +6,7 @@ const createDrinkOrder = async (req, res) => {
   try {
     const drinkOrder = { userId, drinkId, quantity, unitaryPrice, totalPrice };
     const newDrinkOrder = await postDrinkOrder(drinkOrder);
-    mailCreate(drinkId, userId);
+
     return res.status(201).json(newDrinkOrder);
   } catch (error) {
     return res.status(200).json({ error: error.message });
