9c156b2a84d4c5e5cf8361121e1c0a5caca5c281
Marcos Gallardi <gallardimarcos98@gmail.com>
2023-07-27 11:33:08 -0300

    integracion de web hook

--- src/Controllers/postMP.js
+++ src/Controllers/postMP.js
@@ -1,6 +1,6 @@
 const mercadopago = require("mercadopago");
 
-const postMP = async (title, description, unit_price, quantity) => {
+const postMP = async (id,picture_url,title, description, unit_price, quantity) => {
   mercadopago.configure({
     access_token:
       "TEST-840963076660337-072117-1b995a17b690f7df7a5adf4428a413ac-639906523",
@@ -9,23 +9,26 @@ const postMP = async (title, description, unit_price, quantity) => {
   const preference = {
     items: [
       {
+        id,
         title,
         description,
+        picture_url,
         unit_price: Number(unit_price),
         quantity: Number(quantity),
       },
     ],
     back_urls: {
       //a donde va el cliente una vez que la compra finaliza, poner url de railway
-      success: "http://localhost:3000/success",
-      failure: "http://localhost:3000/failure",
-      pending: "http://localhost:3000/pending",
+      success: "https://pf-front-end-grupo3.vercel.app/",
+      failure: "https://pf-front-end-grupo3.vercel.app/home",
+      pending: "https://pf-front-end-grupo3.vercel.app/home",
     },
     auto_return: "approved",
   };
 
   try {
     let response = await mercadopago.preferences.create(preference);
+    console.log(response)
     return response;
   } catch (error) {
     console.log(error);
--- src/routes/mpRouter.js
+++ src/routes/mpRouter.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
 const  mpHandler  = require("../Handlers/mpHandler");
+const webHookMP = require("../Notification/webHookMP")
 const mpRouter = Router()
  
 mpRouter.post('/',mpHandler)
+
+mpRouter.post('/notification',webHookMP)
 
 module.exports = mpRouter;
