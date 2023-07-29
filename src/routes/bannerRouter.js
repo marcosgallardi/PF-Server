c9f92edd44d63e7069f662634294065d305ad8da
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-07-28 22:45:53 -0300

    delete de banner

--- src/routes/bannerRouter.js
+++ src/routes/bannerRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const banner = require("../Handlers/banner");
 const createBanner = require("../Handlers/createBanner");
 const updateBanner = require("../Handlers/updateBanner");
+const deleteBanner = require("../Handlers/deleteBanner");
 
 const bannerRouter = Router();
 
 bannerRouter.get("/", banner);
 bannerRouter.post("/", createBanner);
 bannerRouter.put("/:id", updateBanner);
+bannerRouter.delete("/:id", deleteBanner);
 
 module.exports = bannerRouter;
