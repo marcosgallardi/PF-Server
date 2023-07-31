d5c7bc68824049fefa5b83b3ea8043d009b9f2d6
lenny-dev <104114128+lennycba@users.noreply.github.com>
2023-07-30 23:00:28 -0300

    prueba 3

--- src/Notification/webHookMP.js
+++ src/Notification/webHookMP.js
@@ -3,6 +3,22 @@ const mercadopago = require('mercadopago');
 
 mercadopago.configurations.setAccessToken("TEST-840963076660337-072117-1b995a17b690f7df7a5adf4428a413ac-639906523");
 
+let payment_data = {
+  transaction_amount: Number(req.body.transactionAmount),
+  token: req.body.token,
+  description: req.body.description,
+  installments: Number(req.body.installments),
+  payment_method_id: req.body.paymentMethodId,
+  issuer_id: req.body.issuer,
+  payer: {
+    email: req.body.email,
+    identification: {
+      type: req.body.docType,
+      number: req.body.docNumber
+    }
+  }
+ };
+
   mercadopago.payment.save(payment_data)
   .then(function(response) {
     res.status(response.status).json({
