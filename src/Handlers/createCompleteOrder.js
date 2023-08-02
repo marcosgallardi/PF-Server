698a21b19980d610d787f89c08b58aacb30b0d74
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-08-01 22:14:15 -0300

    intento 3 de mp

--- src/Handlers/createCompleteOrder.js
+++ src/Handlers/createCompleteOrder.js
@@ -1,31 +1,24 @@
 const postCompleteOrder = require("../Controllers/postCompleteOrder");
 const getByIdOrder = require("../Controllers/getByIdOrders");
 const getUserIdFromDatabase = require("../functions/getUserIdByEmail");
-const webHookMP = require("../Notification/webHookMP");
 
 const createCompleteOrder = async (req, res) => {
   const { order } = req.body;
 
   try {
-    // const status = await webHookMP();
-    const status = await webHookMP();
-    console.log("STATUS _______________________", status);
     let userId = "";
-    if (status === "approved") {
-      if (req.user.source === "firebase") {
-        userId = await getUserIdFromDatabase(req.user.email);
-      } else if (req.user.source === "database") {
-        userId = req.user.userId;
-      } else {
-        res.status(400).json({ error: "Error al obtener el usuario" });
-      }
 
-      const newCompleteOrder = await postCompleteOrder({ order, userId });
-
-      // console.log('ORDER HANDLER CREATE__', newCompleteOrder);
-
-      return res.status(201).json(newCompleteOrder);
+    if (req.user.source === "firebase") {
+      userId = await getUserIdFromDatabase(req.user.email);
+    } else if (req.user.source === "database") {
+      userId = req.user.userId;
+    } else {
+      res.status(400).json({ error: "Error al obtener el usuario" });
     }
+
+    const newCompleteOrder = await postCompleteOrder({ order, userId });
+
+    return res.status(201).json(newCompleteOrder);
   } catch (error) {
     return res.status(200).json({ error: error.message });
   }
