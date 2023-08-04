a301f018630e54aa2e306ab248e2ca18567e7a1d
tatoclemente <tatoapuros@gmail.com>
2023-08-03 21:48:16 -0300

    Elimine archivo json con las credenciales de firebase y use esas credenciales desde las variables de entorno

--- src/Notification/webHookMP.js
+++ src/Notification/webHookMP.js
@@ -1,28 +1,5 @@
-// const axios = require("axios");
-
-// const webHookMP = async (req, res) => {
-//   try {
-//     const mpResponse = await axios.get(`https://api.mercadopago.com/v1/payments/${req.body.data.id}`, {
-//       headers: {
-//         Authorization: `Bearer TEST-840963076660337-072117-1b995a17b690f7df7a5adf4428a413ac-639906523`,
-//       },
-//     });
-//     console.log("CONSTANTE COPADAAAAAAAAAAAAA", mpResponse.data);
-//     return mpResponse.data.status;
-//   } catch (error) {
-//     console.error("Error:", error.message);
-//   }
-
-//   res.status(200).send("ok");
-// };
-
-// module.exports = webHookMP;
-
 const axios = require("axios");
-const ticketUpdate = require("../Controllers/putPedido");
-const { User, Ticket } = require("../db");
-const ticketByUserId = require("../Controllers/ticketByUserId");
-const getUserIdFromDatabase = require("../functions/getUserIdByEmail");
+const { Ticket } = require("../db");
 
 const webHookMP = async (req, res) => {
   try {
@@ -32,6 +9,7 @@ const webHookMP = async (req, res) => {
       },
     });
     console.log("TITULOOOOOOOOOOOOOOOOOO", mpResponse.data.title);
+
     const idPedido = mpResponse.data.description.split("-")[1];
     const ticketUpdate = await Ticket.findOne({ where: { idPedido: idPedido } });
     if (mpResponse.data.status === "approved") {
@@ -42,7 +20,7 @@ const webHookMP = async (req, res) => {
 
     await ticketUpdate.save();
 
-    console.log("CONSTANTE COPADAAAAAAAAAAAAA", mpResponse.data.status, "", mpResponse.data.payer);
+    console.log("CONSTANTE COPADAAAAAAAAAAAAA", mpResponse.data.status);
   } catch (error) {
     console.error("Error:", error.message);
   }
